b857627ec65c54b0c36a71cb1ae8d1bf142097d1
kattamanjusri <kattamanjusri@gmail.com>
2025-05-11 06:29:15 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/home', // important for Netlify routing
+  base: '/', // important for Netlify routing
 });
